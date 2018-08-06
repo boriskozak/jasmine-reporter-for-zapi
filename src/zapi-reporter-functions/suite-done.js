b27c1a27149636b4747435b1ff7179ccc10d3e60
Boris Kozak <bk@jibe.com>
2018-08-06 13:41:00 -0400

    bugfix - make sure all pending tasks are complete when the suite finishes

--- src/zapi-reporter-functions/suite-done.js
+++ src/zapi-reporter-functions/suite-done.js
@@ -4,30 +4,20 @@ module.exports = function() {
         return;
     }
 
-
     Promise.all(this.suitePromises).then(() => {
         this.zapiService.updateExecutionStatus(
             this.globals.executionId,
             this.globals.issueKey,
             this.globals.projectId,
-            this.globals.status,
-            () => {
+            this.globals.status)
+            .then(() => {
                 if (this.onCompleteDefer.resolve) {
                     this.onCompleteDefer.resolve();
                 } else {
                     this.onCompleteDefer.fulfill();
                 }
             },
-            (error) => {
-                console.error(error);
-                if (this.onCompleteDefer.resolve) {
 
-                    this.onCompleteDefer.resolve();
-                } else {
-
-                    this.onCompleteDefer.fulfill();
-                }
-            }
         );
     });
 
